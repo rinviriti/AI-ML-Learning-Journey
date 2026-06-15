d6568633eb87a9aecc84d26aa573badec0920cd9
Rinvi Jaman Riti <155306221+rinviriti@users.noreply.github.com>
2026-06-15 11:54:10 +0600

    Add variables for name, department, and university

---
+++ 01_python_fundamentals/variables.py
@@ -0,0 +1,7 @@
+name = "Rinvi Jaman Riti"
+department = "Computer Science and Engineering"
+university = "Daffodil International University"
+
+print("Name:", name)
+print("Department:", department)
+print("University:", university)
