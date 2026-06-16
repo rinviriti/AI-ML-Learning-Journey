468a079a38e00bea6dfc9a7f863d989ddc445de1
Rinvi Jaman Riti <155306221+rinviriti@users.noreply.github.com>
2026-06-16 12:09:06 +0600

    Add arithmetic operators example

---
+++ 01_python_fundamentals/operators.py
@@ -0,0 +1,10 @@
+# Arithmetic Operators
+
+a = 10
+b = 5
+
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+print("Modulus:", a % b)
