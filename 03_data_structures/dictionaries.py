27b30b3da9a0ad7ae369c84456a96da6dfdba88b
Rinvi Jaman Riti <155306221+rinviriti@users.noreply.github.com>
2026-06-16 12:32:37 +0600

    Add example of dictionaries in Python

---
+++ 03_data_structures/dictionaries.py
@@ -0,0 +1,12 @@
+# Dictionaries in Python
+
+student = {
+    "name": "Riti",
+    "department": "CSE",
+    "cgpa": 3.86
+}
+
+print(student)
+
+print("Name:", student["name"])
+print("CGPA:", student["cgpa"])
