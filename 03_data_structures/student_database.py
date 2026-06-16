43d2ec3e04ea1d7e49543f168c428df6db7122f4
Rinvi Jaman Riti <155306221+rinviriti@users.noreply.github.com>
2026-06-16 12:33:26 +0600

    Add student database with sample data

---
+++ 03_data_structures/student_database.py
@@ -0,0 +1,9 @@
+# Student Database
+
+students = {
+    1: {"name": "Riti", "cgpa": 3.86},
+    2: {"name": "Rinvi", "cgpa": 3.85}
+}
+
+for student_id, info in students.items():
+    print(student_id, info)
