76ca4e9cb7f950809a1d30bb212b104d48dd811c
Rinvi Jaman Riti <155306221+rinviriti@users.noreply.github.com>
2026-06-16 12:10:59 +0600

    Add nested if example

---
+++ 01_python_fundamentals/nested_if.py
@@ -0,0 +1,13 @@
+# Nested If Example
+
+marks = 85
+
+if marks >= 50:
+    print("Pass")
+
+    if marks >= 80:
+        print("Excellent")
+    else:
+        print("Good")
+else:
+    print("Fail")
